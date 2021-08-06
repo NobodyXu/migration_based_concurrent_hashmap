1f574017ad035bd98582731c539745ad3644f692
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-06 11:50:42 +1000

    Extract new mod swiss_like_table
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,16 @@ extern crate bitvec;
 extern crate init_array;
 extern crate static_assertions;
 
+#[cfg(test)]
+extern crate assert_matches;
+
 use static_assertions::{const_assert, const_assert_eq};
 
 pub use arc_swap::RefCnt;
 
-#[cfg(test)]
-extern crate assert_matches;
-
 mod utility;
 
 mod value;
-
 pub use value::SlotIndex;
 
-mod slot;
+pub mod swiss_like_table;
